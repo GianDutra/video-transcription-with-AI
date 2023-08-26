f686c0c362de6c63e5053236df60e433b2e83aba
GianDutra <chikegang@gmail.com>
2023-08-26 10:17:40 -0300

    .

--- src/form.js
+++ src/form.js
@@ -18,7 +18,7 @@ form.addEventListener("submit", async e => {
         await loadVideo(url);
 
         loadingMessage("Conectando com o backend");
-        await axios.get('https://video-transcription-ai.onrender.com/audio?v=' + getVideoId(url));
+        await axios.get('http://localhost:3000//audio?v=' + getVideoId(url));
 
         const data = await transcribeAudio();
         renderText(data);
